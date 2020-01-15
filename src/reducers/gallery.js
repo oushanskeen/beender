2772aef02a59b760c9ab4397bb6f700753149978
Oushan Skeen <inta.soul@gmail.com>
2020-01-15 15:12:21 +0300

    Add action reducers:
    - Write tests for action reducers.
    - Implement action reducers.

--- src/reducers/gallery.js
+++ src/reducers/gallery.js
@@ -1,7 +1,33 @@
+import {
+  LOVE,
+  HATE
+} from '../constants/ActionTypes'
+
 const initialState = (window.Cypress && window.initialState) || [
-  {id:"1",status:'unknown'}
+  {id:"1",status:'unknown'},
+  {id:"2",status:'unknown'},
+  {id:"3",status:'unknown'},
+  {id:"4",status:'unknown'},
+  {id:"5",status:'unknown'},
+  {id:"6",status:'unknown'},
+  {id:"7",status:'unknown'},
 ];
 
 export default function gallery(state = initialState, action) {
-    return state;
-};
+  switch (action.type) {
+    case LOVE:
+      return state.map(gallery => 
+        (gallery.id === action.id 
+            ? { ...gallery, status:"Love"} 
+            : gallery)
+        );
+    case HATE:
+      return state.map(gallery => 
+        (gallery.id === action.id 
+            ? { ...gallery, status:"Hate"} 
+            : gallery)
+        );   
+    default:
+      return state
+  }
+}
--- src/reducers/gallery.spec.js
+++ src/reducers/gallery.spec.js
@@ -1,12 +1,47 @@
 import gallery from './gallery';
+import * as types from '../constants/ActionTypes';
 
 describe('gallery reducer', () => {
   it('should handle initial state', () => {
     expect(
       gallery(undefined, {})
     ).toEqual([
-        {id:"1",status:'unknown'}
+      {id:"1",status:'unknown'},
+      {id:"2",status:'unknown'},
+      {id:"3",status:'unknown'},
+      {id:"4",status:'unknown'},
+      {id:"5",status:'unknown'},
+      {id:"6",status:'unknown'},
+      {id:"7",status:'unknown'},
     ]);
   });
-});
 
+  it('should handle LOVE', () => {
+    expect(
+      gallery([{id:"1",status:'unknown'}], {
+        id: "1",
+        type: types.LOVE
+      })
+    ).toEqual([
+      {
+        id: "1",
+        status:"Love"
+      }
+    ]);
+  });
+
+  it('should handle HATE', () => {
+    expect(
+      gallery([{id:"1",status:'unknown'}], {
+        id: "1",
+        type: types.HATE
+      })
+    ).toEqual([
+      {
+        id: "1",
+        status:"Hate"
+      }
+    ]);
+  });
+
+});
