c29c99f21a732baf83ce5947d1fcb33bdc5d94b8
Oushan Skeen <inta.soul@gmail.com>
2020-01-22 19:05:11 +0300

    Updates

---
+++ precache-manifest.776419f32677294adc219f1a545b560d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8335f702b7dbce72496adeb95497d42c",
+    "url": "/beender/index.html"
+  },
+  {
+    "revision": "c0e8864e81c253510103",
+    "url": "/beender/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9424a9085255fd4a9057",
+    "url": "/beender/static/js/2.57af05be.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/beender/static/js/2.57af05be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0e8864e81c253510103",
+    "url": "/beender/static/js/main.9d26d113.chunk.js"
+  },
+  {
+    "revision": "2c1d29990622cc51a285",
+    "url": "/beender/static/js/runtime-main.43ab1129.js"
+  }
+]);
