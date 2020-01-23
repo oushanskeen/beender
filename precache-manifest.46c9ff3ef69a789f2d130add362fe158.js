eeffc9ca12c97f350b4171cb93d73b49a48c70cf
Oushan Skeen <inta.soul@gmail.com>
2020-01-23 19:19:11 +0300

    Updates

--- precache-manifest.46c9ff3ef69a789f2d130add362fe158.js
+++ precache-manifest.46c9ff3ef69a789f2d130add362fe158.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7e9f44ba8bece09c3ed2a389bb0f4266",
+    "revision": "508cb3b1808d4ee0a839817685c25daa",
     "url": "/beender/index.html"
   },
   {
-    "revision": "f8a5afbea1c18fc713d7",
+    "revision": "a06c23b64d70d7bdac4d",
     "url": "/beender/static/css/main.d1b05096.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/beender/static/js/2.57af05be.chunk.js.LICENSE"
   },
   {
-    "revision": "f8a5afbea1c18fc713d7",
-    "url": "/beender/static/js/main.4a1b6e5b.chunk.js"
+    "revision": "a06c23b64d70d7bdac4d",
+    "url": "/beender/static/js/main.5f566458.chunk.js"
   },
   {
     "revision": "2c1d29990622cc51a285",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/beender/precache-manifest.5fba5e50531422ec17748dd288e3d098.js"
+  "/beender/precache-manifest.46c9ff3ef69a789f2d130add362fe158.js"
 );
 
 self.addEventListener('message', (event) => {
