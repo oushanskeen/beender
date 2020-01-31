73a9ee5be8267981637687624acdc95089759cd1
Oushan Skeen <inta.soul@gmail.com>
2020-01-31 16:19:03 +0300

    BEENDER-152: 4.Create reducers for every action:
    - Add test for reducer(s).
    - Implement reducer(s).

--- cypress/integration/orderPage.spec.js
+++ cypress/integration/orderPage.spec.js
@@ -13,7 +13,19 @@ context("The Order Page", ()=>{
              .its('store')
              .invoke('getState')
              .should('deep.equal',
-                {outcome:" "}
+                {
+                    gallery:[
+                        {id:"1",status:'unknown'},
+                        {id:"2",status:'unknown'},
+                        {id:"3",status:'unknown'},
+                        {id:"4",status:'unknown'},
+                        {id:"5",status:'unknown'},
+                        {id:"6",status:'unknown'},
+                        {id:"7",status:'unknown'}
+                    ],
+                    choice:{isSelected:" "},
+                    selection:{outcome:" "}   
+                }
              )
       });   
 });
--- src/reducers/order.js
+++ src/reducers/order.js
@@ -1,8 +1,14 @@
+import {
+  HOME
+} from '../constants/ActionTypes'
 
 const initialState = (window.Cypress && window.initialState) || {outcome: " " };
 
-export default function selected(state=initialState, action) {  
-    default:
-      return state
+export default function selected(state=initialState, action) {
+    switch (action.type) {
+        case HOME:
+          return[{}];    
+        default:
+          return state
   }
 };
