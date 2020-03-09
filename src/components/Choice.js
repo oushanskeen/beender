6ee80f0f83631c423780c9c86e65dbebbfeb3db2
Oushan Skeen <inta.soul@gmail.com>
2020-03-10 01:51:00 +0300

    Add style to Choice component

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import '../App.css';
+import '../css/Choice.css';
 import { connect } from "react-redux";
 import * as actions from '../actions';
 import { Link } from 'react-router-dom';
 
 let Choice = ({ isLoved, isSelected, onSelect}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>You love: </p>
-        <div id='choice'>
+        <div id='choice' class="ChoiceBox">
+            <div class="heartSignBox cell">HEART</div>
+                <div class="selectionAreaBox cell">
+                    <div class="picArea cell">
             {
                 isLoved.map(e => 
                     <Link to={`/beender/choice/:${e.id}`} 
+                        class="picItem cell"
                         onClick={()=>onSelect(e.id)}><p>{e.id}</p>
                     </Link>)
             }
+            </div>
+            </div>
+        <p class="descriptionAreaBox cell"> You are in a world of chosen cards </p>
         </div>
-        <p> You are in a world of chosen cards </p>
-      </header>
-    </div>
+        
   );
 };
 
