3fa3791e96866bdfb2f3da2014b117c08e9f308c
Oushan Skeen <inta.soul@gmail.com>
2020-03-10 03:06:39 +0300

    Add style to Selection component

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../App.css';
+import '../css/Selection.css';
 import { connect } from "react-redux";
 import * as actions from '../actions';
 import { Link, useParams } from 'react-router-dom';
@@ -8,29 +8,40 @@ let Selection = ({ selection, info=[{id:"1",status:'unknown'}], onNo, onYes}) =>
   let {beerId} = useParams();
   let essence = info.filter(e => e.id === beerId[1]);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>You are in a world of your selections</p>
-        <div id="selection">
-            <p id="selectedCard">
+return (
+    <div id="selection" class="SelectionBox">
+        <div class="contentAreaBox">
+            <div id="selectedCard" class="choicePicArea cell">
                 id: {essence[0].id}, 
-                status: {essence[0].status}</p>
-            <Link to='/beender/choice' id='noButton' 
-                onClick={()=>onNo()}
-            > 
-                NO 
-            </Link>
-            <span> </span>
-            <Link to='/beender/order' id='yesButton' 
-                onClick={()=>onYes(beerId[1])}
-            > 
-                YES  
-            </Link> 
-        </div>
-      </header>
+                status: {essence[0].status}
+            </div>
+            <div class="descrAreaBox">
+                <div class="descrTextArea cell">
+                    some text   
+                </div>
+                <div class="descrButtonArea">
+                    <Link 
+                        to='/beender/choice' 
+                        id='noButton' 
+                        class="noChoiceButton cell"
+                        onClick={()=>onNo()}
+                    > 
+                        NO 
+                    </Link>
+            
+                    <Link 
+                        to='/beender/order' 
+                        id='yesButton' 
+                        class="yesChoiceButton cell"
+                        onClick={()=>onYes(beerId[1])}
+                    > 
+                        YES  
+                    </Link>
+                </div>
+            </div>
+        </div> 
     </div>
-  );
+);
 };
 
 const mapStateToProps = state => ({
